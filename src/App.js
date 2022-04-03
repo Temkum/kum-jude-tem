49defe3bc78c0067aebd49c0aef97868f78ad4cd
Kum Jude Tem <judekum14@gmail.com>
2022-04-03 22:49:06 +0100

    Initial commit & created components

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from "react";
+import Header from "./components/header/Header";
+import Nav from "./components/nav/Nav";
+
+function App() {
+  return (
+    <>
+      <Header />
+      <Nav />
+    </>
+  );
+}
+
+export default App;
---
+++ src/components/nav/Nav.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+function nav() {
+  return (
+    <div>
+      <h2>This is the nav</h2>
+    </div>
+  );
+}
+
+export default nav;
