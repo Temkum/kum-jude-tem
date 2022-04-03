e41bc6c359719733efbf26e0720f8ca1a2bb57e7
Kum Jude Tem <judekum14@gmail.com>
2022-04-04 00:15:51 +0100

    Added other components & used useState on nav component

--- Todo.md
+++ Todo.md
@@ -2,3 +2,7 @@
 
 - Built the nav
 - include bootstrap icons `npm i bootstrap-icons --save` to use as a dependency
+
+## Deployment
+
+- To deploy using netlify `npm install netlify-cli -g` then run `netlify build`
--- src/components/nav/Nav.js
+++ src/components/nav/Nav.js
@@ -4,34 +4,61 @@ import {
   CodeSquare,
   Folder,
   House,
-  Clipboard2Data,
+  ColumnsGap,
   Envelope,
   Person,
 } from "react-bootstrap-icons";
+import { useState } from "react";
 
-function nav() {
+function Nav() {
+  const [activeNav, setActiveNav] = useState("#");
+
   return (
     <nav>
-      <a href="#">
+      <a
+        href="#"
+        className={activeNav === "#" ? "active" : ""}
+        onClick={() => setActiveNav("#")}
+      >
         <House />
       </a>
-      <a href="#services">
-        <Clipboard2Data />
+      <a
+        href="#services"
+        onClick={() => setActiveNav("#services")}
+        className={activeNav === "#services" ? "active" : ""}
+      >
+        <ColumnsGap />
       </a>
-      <a href="#technologies">
+      <a
+        href="#technologies"
+        onClick={() => setActiveNav("#technologies")}
+        className={activeNav === "#technologies" ? "active" : ""}
+      >
         <CodeSquare />
       </a>
-      <a href="#portfolio">
+      <a
+        href="#portfolio"
+        onClick={() => setActiveNav("#portfolio")}
+        className={activeNav === "#portfolio" ? "active" : ""}
+      >
         <Folder />
       </a>
-      <a href="#contact">
+      <a
+        href="#contact"
+        onClick={() => setActiveNav("#contact")}
+        className={activeNav === "#contact" ? "active" : ""}
+      >
         <Envelope />
       </a>
-      <a href="#about">
+      <a
+        href="#about"
+        onClick={() => setActiveNav("#about")}
+        className={activeNav === "#about" ? "active" : ""}
+      >
         <Person />
       </a>
     </nav>
   );
 }
 
-export default nav;
+export default Nav;
