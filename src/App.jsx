8601f4fd796feaafd59d0ce350b70ea8f75e4f2b
Kum Jude Tem <judekum14@gmail.com>
2022-04-04 23:12:30 +0100

    Technology component modified

--- src/App.jsx
+++ src/App.jsx
@@ -12,9 +12,9 @@ function App() {
     <>
       <Header />
       <Nav />
+      <Technologies />
       <Services />
       <Portfolio />
-      <Technologies />
       <Testimonies />
       <Footer />
     </>
