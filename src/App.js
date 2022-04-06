fe7c4dadcc238b5ca9eacaa6b0747dcd83c30e3e
Kum Jude Tem <judekum14@gmail.com>
2022-04-06 13:34:33 +0100

    Modified App.js extention

---
+++ src/App.js
@@ -0,0 +1,26 @@
+import React from "react";
+import Header from "./components/header/Header";
+import Nav from "./components/nav/Nav";
+import Services from "./components/services/Services";
+import Portfolio from "./components/portfolio/Portfolio";
+import Technologies from "./components/technologies/Technologies";
+import Testimonies from "./components/testimonies/Testimonies";
+import Contact from "./components/contact/Contact";
+import Footer from "./components/footer/Footer";
+
+function App() {
+  return (
+    <>
+      <Header />
+      <Nav />
+      <Technologies />
+      <Services />
+      <Portfolio />
+      <Testimonies />
+      <Contact />
+      <Footer />
+    </>
+  );
+}
+
+export default App;
